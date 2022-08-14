62365bf7acdccf0be56b7d7b105109dc78d3d637
1 <1@qq.com>
2022-08-14 23:32:44 +0800

    conv新增图片转换判断与新方法映射

--- src/image/conv.ts
+++ src/image/conv.ts
@@ -4,9 +4,11 @@ import { blobResize, blobToFile, getFileNameSuffix, imgFileGetBlob } from './too
 export interface webpConvResult {
   /* 原始上传的文件 */
   originFile: File;
-  /* 原始文件target 可以获取宽高*/
+  /* 原始文件target 可以获取原始文件宽高*/
   originTarget: HTMLImageElement;
-  /* 原始文件转换为webp后的文件  */
+  /* 压缩原始文件后的target 可以获取压缩后宽高*/
+  compressTarget?: HTMLImageElement;
+  /* 原始文件转换为webp后的文件 可能被压缩 看传入的参数  */
   webp: File;
   /* 原始文件生成的预览图webp文件  */
   previewWebp: File;
@@ -46,16 +48,19 @@ export const imageToWebp = async (
   const previewName = `${name}${previewSuffix}.webp`;
   let origin = await imgFileGetBlob(file);
   const ot = { ...origin };
+  let runCompress = false;
   if (originMaxWidth) {
     if (origin.target.width > originMaxWidth || origin.target.height > originMaxWidth) {
       const originFile = await blobResize(origin.blob!, originName, originMaxWidth);
       origin = await imgFileGetBlob(originFile);
+      runCompress = true;
     }
   }
   const originWebpFile = blobToFile(origin.blob!, originName, origin.mimeType);
   const thumbnail_file = await blobResize(origin.blob!, previewName, previewMax!);
   return {
     originTarget: ot.target,
+    compressTarget: runCompress ? origin.target : undefined,
     originFile: file,
     webp: originWebpFile,
     previewWebp: thumbnail_file,
